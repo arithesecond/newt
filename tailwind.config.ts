6fb9cab8c9439af91df7244a9341f46257f7bdef
April Hall <ari@arithefirst.com>
2025-03-01 18:08:23 -0500

    feat: Setup dynamic colorscheme from config

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+import siteConfig from './config';
+
+const config: Config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        base: siteConfig.colors.baseBg,
+        dark: siteConfig.colors.darkBg,
+        accent: siteConfig.colors.accent,
+        primarytext: siteConfig.colors.textPrimary,
+        secondarytext: siteConfig.colors.textSecondary
+      },
+    },
+  },
+};
+
+export default config;
